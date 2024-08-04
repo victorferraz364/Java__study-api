df85b2ee188e80af5e8063ad9fbc456db3abab67
unknown <victorferraz364@gmail.com>
2024-08-04 20:07:51 -0300

    inicio projeto

---
+++ src/main/java/victordev/studi_api/StudiApiApplication.java
@@ -0,0 +1,13 @@
+package victordev.studi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/victordev/studi_api/StudiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package victordev.studi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
